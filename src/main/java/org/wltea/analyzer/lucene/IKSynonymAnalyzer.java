ca8d9def88f5c12292206b14aa12b2b5e64e5397
ranger <jijunlx@gmail.com>
2014-02-18 17:32:11 +0800

    add synonyms dictionary base on ik-analyzer

---
+++ src/main/java/org/wltea/analyzer/lucene/IKSynonymAnalyzer.java
@@ -0,0 +1,60 @@
+package org.wltea.analyzer.lucene;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.synonym.SynonymFilterFactory;
+import org.apache.lucene.analysis.util.ClasspathResourceLoader;
+import org.apache.lucene.analysis.util.ResourceLoader;
+import org.apache.lucene.util.Version;
+import org.wltea.analyzer.cfg.Configuration;
+import org.wltea.analyzer.cfg.DefaultConfig;
+
+/**
+ * 实现同义词功能
+ * 
+ * @author ranger
+ * 
+ */
+public class IKSynonymAnalyzer extends Analyzer {
+
+    private final boolean useSmart;
+    private final Version luceneMatchVersion;
+
+    public IKSynonymAnalyzer(Version version) {
+        this(version, false);
+    }
+
+    public IKSynonymAnalyzer(Version version, boolean useSmart) {
+        this.luceneMatchVersion = version;
+        if (!version.onOrAfter(Version.LUCENE_40)) {
+            throw new IllegalArgumentException("This class only works with Lucene 4.0+.");
+        }
+        this.useSmart = useSmart;
+    }
+
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName, Reader reader) {
+        Tokenizer token = new IKTokenizer(reader, useSmart);
+        Map<String, String> paramsMap = new HashMap<String, String>();
+        Configuration cfg = DefaultConfig.getInstance();
+
+        paramsMap.put("luceneMatchVersion", luceneMatchVersion.toString());
+        paramsMap.put("synonyms", cfg.getExtSynonymDictionarys());
+        paramsMap.put("ignoreCase", "true");
+        SynonymFilterFactory factory = new SynonymFilterFactory(paramsMap);
+        ResourceLoader loader = new ClasspathResourceLoader();
+        try {
+            factory.inform(loader);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new TokenStreamComponents(token, factory.create(token));
+    }
+
+}
